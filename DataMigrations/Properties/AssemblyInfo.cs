d8245e05ab1c82d2a2ec9bef1cb28789c96ae9a8
michal-ciechan <lndcobra@hotmail.com>
2016-05-10 14:47:15 +0100

    Added NuSpect

--- DataMigrations/Properties/AssemblyInfo.cs
+++ DataMigrations/Properties/AssemblyInfo.cs
@@ -6,12 +6,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataMigrations")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DataMigrations")]
+[assembly:
+    AssemblyDescription(
+        "A Fluent SQL Generator which allows you to generate Insert, Update and Delete SQL Statements."
+        )]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michal Ciechan @ Codeperf.net")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Codeperf")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
