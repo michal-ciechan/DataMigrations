21e63104b05299137258107bc6c0079195ba9b9e
michal-ciechan <lndcobra@hotmail.com>
2016-06-02 23:46:15 +0100

    Fixing DataNotations Ef Tests

--- DataMigrations.IntegrationTests.EF/Person.cs
+++ DataMigrations.IntegrationTests.EF/Person.cs
@@ -1,10 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
+﻿
 namespace DataMigrations.IntegrationTests.EF
 {
     public class Person
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
     }
